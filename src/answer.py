ba77f4cc7a35b8138db1ce60cf7febc321ab7a2b
ryk0z <olivier.sportif@free.fr>
2017-05-05 19:07:40 +0200

    Answer.py avance

--- src/answer.py
+++ src/answer.py
@@ -1,7 +1,36 @@
 
 import re
+import random
 
 answers = {}
 
 answers["HI"] = ["Hello !", "Nice to meet you !", "Hi !"]
 
+answers["BYE"] = ["Good bye !", "Bye !"]
+
+answers["YES"] = ["Hello !", "Nice to meet you !", "Hi !"]
+
+answers["NO"] = ["Hello !", "Nice to meet you !", "Hi !"]
+
+answers["MISTAKE"] = ["Hello !", "Nice to meet you !", "Hi !"]
+
+answers["HIBOOL"] = [False]
+
+
+def answer(data,modified = None):
+
+""" Possible que data contienne une clé "special" contenant :
+hi,bye,yes,no,mistake
+"""
+	answer = ""
+
+	if "special" in data:
+		if data["special"] == "bye":
+			answer += random.choice(answers["BYE"].values())
+			return False;
+		if data["special"] == "hi" and answers["HIBOOL"] == False:
+			answer += random.choice(answers["HI"].values())
+			answers["HIBOOL"] = True
+
+
+	return True
