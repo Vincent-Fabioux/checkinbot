5f2057c8aea1495c1ba4e66f96988ec8cd0ad32e
ryk0z <olivier.sportif@free.fr>
2017-05-05 19:17:29 +0200

    answerDebug

--- src/answer.py
+++ src/answer.py
@@ -14,23 +14,31 @@ answers["NO"] = ["Hello !", "Nice to meet you !", "Hi !"]
 
 answers["MISTAKE"] = ["Hello !", "Nice to meet you !", "Hi !"]
 
-answers["HIBOOL"] = [False]
+answers["HIBOOL"] = False
 
 
 def answer(data,modified = None):
 
-""" Data can contain a key named 'special' with values :
-hi,bye,yes,no,mistake
-"""
-	answer = ""
+#Data can contain a key named 'special' with values hi,bye,yes,no,mistake
 
-	if "special" in data:
-		if data["special"] == "bye":
-			answer += random.choice(answers["BYE"].values())
-			return False;
-		if data["special"] == "hi" and answers["HIBOOL"] == False:
-			answer += random.choice(answers["HI"].values())
-			answers["HIBOOL"] = True
+  answer = ""
+
+  if "special" in data:
+    if data["special"] == "bye":
+      answer += random.choice(answers["BYE"])
+      return False;
+    if data["special"] == "hi" and answers["HIBOOL"] == False:
+      answer = random.choice(answers["HI"])
+      answers["HIBOOL"] = True
+
+    print(answer)
+
+  return True
 
 
-	return True
+def answerDebug():
+  data = {"dep_loc": None, "dep_hour": None,
+      "arr_loc": None, "arr_hour": None, "special":"hi"}
+  Res = answer(data)
+
+answerDebug()
